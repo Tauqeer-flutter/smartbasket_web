c4657614eb6d20301b9e28dd77e83d980d5189ae
Tauqeer-flutter <tauqeer.flutter@tafsol.com>
2026-06-17 17:39:19 +0500

    update - initial project

---
+++ lib/assets/assets.dart
@@ -0,0 +1,4 @@
+final class SvgAssets {
+  static const String _assets = 'assets';
+  static const String logo = '$_assets/svg/logo.svg';
+}
---
+++ lib/components/header.dart
@@ -0,0 +1,66 @@
+import 'package:jaspr/dom.dart';
+import 'package:jaspr/jaspr.dart';
+
+import '../constants/theme.dart';
+
+class Header extends StatelessComponent {
+  const Header({super.key});
+
+  @override
+  Component build(BuildContext context) {
+    return header([
+      img(src: src),
+      Component.text('Saboor'),
+      Component.text('Haider'),
+    ]);
+  }
+
+  @css
+  static List<StyleRule> get styles => [
+    css('header', [
+      css('&').styles(
+        display: .flex,
+        padding: .symmetric(
+          vertical: 27.em,
+          horizontal: 120.em,
+        ),
+        justifyContent: .center,
+      ),
+      css('nav', [
+        css('&').styles(
+          display: .flex,
+          height: 3.em,
+          radius: .all(.circular(10.px)),
+          overflow: .clip,
+          justifyContent: .spaceBetween,
+          backgroundColor: primaryColor,
+        ),
+        css('a', [
+          css('&').styles(
+            display: .flex,
+            height: 100.percent,
+            padding: .symmetric(horizontal: 2.em),
+            alignItems: .center,
+            color: Colors.white,
+            fontWeight: .w700,
+            textDecoration: TextDecoration(line: .none),
+          ),
+          css('&:hover').styles(
+            backgroundColor: const Color('#0005'),
+          ),
+        ]),
+        css('div.active', [
+          css('&').styles(position: .relative()),
+          css('&::before').styles(
+            content: '',
+            display: .block,
+            position: .absolute(bottom: 0.5.em, left: 20.px, right: 20.px),
+            height: 2.px,
+            radius: .circular(1.px),
+            backgroundColor: Colors.white,
+          ),
+        ]),
+      ]),
+    ]),
+  ];
+}
